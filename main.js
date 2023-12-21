33d9df2daf2df14b819ee904f17bd2e5dd165b5d
matishadow <matishadow@gmail.com>
2023-12-21 20:55:23 +0100

    Add version 1

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿function updateTimer() {
+    past  = Date.parse("November 11, 2008 11:30:00");
+    now     = new Date();
+    diff    = now - past;
+
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+
+    document.getElementById("timer")
+        .innerHTML =
+        '<div style="margin-bottom: 6rem">' + '<span>Iza i Szymon 💘</span></div>' +
+        '<div style="margin-bottom: 3rem">' + d + '<span>dni</span></div>' +
+        '<div style="margin-bottom: 3rem">' + h + '<span>godzin</span></div>' +
+        '<div style="margin-bottom: 3rem">' + m + '<span>minut</span></div>' +
+        '<div style="margin-bottom: 3rem">' + s + '<span>sekund</span></div>' +
+        '<div style="margin-top: 6rem">' + '<span>razem</span></div>';
+}
+setInterval('updateTimer()', 1000 );
